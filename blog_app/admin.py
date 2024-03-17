b169471b1161d3f1509a703f20a811f0f6ec98ca
ArNaPa <130819942+ArNaPa@users.noreply.github.com>
2024-03-17 08:49:00 -0400

    Models Creations Admin Panel Update

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Blog
 
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'autor', 'status', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'autor', 'status')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
 # Register your models here.
