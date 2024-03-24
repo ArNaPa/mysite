fb6870c74d531c9522f32a341a6cd2381a993f2b
ArNaPa <130819942+ArNaPa@users.noreply.github.com>
2024-03-24 01:42:17 -0400

    Dynamic media taking from models

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Blog
 
 
 # Create your views here.
 def blog(request):
     categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True)
     context = {
         'categories': categories,
+        'featured_posts': featured_posts,
     }
     return render(request, 'blog.html', context)
